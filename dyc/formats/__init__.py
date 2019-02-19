d6dfbdc7cf303f11ccd0a76c21fd151ec4ab0095
Mohammad <mohammad@britecore.com>
2019-02-19 20:15:34 +0200

    Rename to defaults

--- dyc/formats/__init__.py
+++ dyc/formats/__init__.py
@@ -2,7 +2,7 @@ import os
 from ..utils import read_config
 from ..exceptions import FormattingConfigurationHandler
 ROOT_PATH = os.getcwd()
-
+DEFAULT = os.path.join(ROOT_PATH, 'dyc', 'formats', 'defaults.yaml')
 
 class ExtensionManager(object):
     @classmethod
@@ -15,9 +15,8 @@ class ExtensionManager(object):
 
 class DefaultConfig(object):
     def __init__(self, filename):
-        curr_path = os.path.join(ROOT_PATH, 'dyc', 'formats')
         extension = filename.split('.')[-1]
-        configs = next(read_config('{}/formats.yaml'.format(curr_path)))
+        configs = next(read_config(DEFAULT))
         try:
             self.config = ExtensionManager.get_format_extension(extension, configs)
         except Exception as e:
