18084a80a62638e985ae30f48bf4066a2304d040
Mohammad <mohammad@britecore.com>
2019-02-22 14:43:21 +0200

    WIP command for diffs

--- dyc/dyc.py
+++ dyc/dyc.py
@@ -1,6 +1,9 @@
+import os
 import click
+import tempfile
 from .parser import Config
 from .main import DYC
+from diff import Diff
 
 config = click.make_pass_decorator(Config, ensure=True)
 
@@ -17,3 +20,27 @@ def start(config, input):
     """Simple program that greets NAME for a total of COUNT times."""
     dyc = DYC(config.options)
     dyc.start()
+
+
+@main.command()
+@config
+def diff(config):
+    """This argument will run DYC on DIFF patch only"""
+    diff = Diff()
+    for index in diff.uncommitted:
+        if index.get('diff'):
+            diff = index.get('diff')
+            name = index.get('name')
+            temp_file = '.dyc.{}'.format(name)
+            # We have a temp file to get the function name from from and see where to document
+            print(index.get('name'), index.get('hunk'))
+            # with open(temp_file, 'w+') as temp:
+            #     try:
+            #         temp.write(index.get('additions'))
+            #         temp.seek(0)
+            #         config.options['files'] = [temp_file]
+            #         dyc = DYC(config.options)
+            #         dyc.start()
+            #     except:
+            #         os.remove(temp_file)
+            #         break
